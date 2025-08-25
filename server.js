613c6b47500bffa9b7b2f40f0986701ac5c2f7e8
DrDixxx <131374665+DrDixxx@users.noreply.github.com>
2025-08-24 21:59:30 -0600

    feat: add node server and ejs views

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index'));
+app.get('/shop', (req, res) => res.render('shop'));
+app.get('/learn', (req, res) => res.render('learn'));
+app.get('/about', (req, res) => res.render('about'));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
